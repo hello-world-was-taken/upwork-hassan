464733dd9c6e126778024405279db327f6c38638
hello_world <haileformal@gmail.com>
2023-11-03 11:27:49 +0300

    make components responsive

--- lib/features/second_milestone/presentation/widgets/side_notes.dart
+++ lib/features/second_milestone/presentation/widgets/side_notes.dart
@@ -6,30 +6,67 @@ class SideNotes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 800) {
+          // Desktop layout
+          return DesktopSideNotes();
+        } else {
+          // Mobile layout
+          return MobileSideNotes();
+        }
+      },
+    );
+  }
+}
+
+class DesktopSideNotes extends StatelessWidget {
+  const DesktopSideNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: _contentBuilder(context),
+    );
+  }
+}
+
+class MobileSideNotes extends StatelessWidget {
+  const MobileSideNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: _contentBuilder(context));
+  }
+}
+
+List<Widget> _contentBuilder(BuildContext context) {
+  return [
+    Container(
+      child: Image.asset("assets/images/phone.png"),
+      width: AppDimension.width(150, context),
+    ),
+    Column(
       children: [
-        Container(
-          child: Image.asset("assets/images/phone.png"),
-          width: AppDimension.relativeWidth(80, context),
-        ),
         SizedBox(height: 30),
         Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: AppDimension.relativeWidth(10, context)),
+          padding:
+              EdgeInsets.symmetric(horizontal: AppDimension.width(10, context)),
           child: Text(
             "あなたのデータは​ハヤランドが完全消去。",
             maxLines: 3,
             softWrap: true,
             textScaleFactor: AppDimension.textScaleFactor(context),
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
         SizedBox(
           height: 30,
         ),
         Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: AppDimension.relativeWidth(10, context)),
+          padding:
+              EdgeInsets.symmetric(horizontal: AppDimension.width(10, context)),
           child: Text(
             "透明な買取システムで安心を提供します。",
             softWrap: true,
@@ -43,7 +80,7 @@ class SideNotes extends StatelessWidget {
         ),
         SizedBox(height: 50),
         Container(
-          width: AppDimension.relativeWidth(80, context),
+          width: AppDimension.width(150, context),
           child: Column(
             children: [
               Padding(
@@ -55,7 +92,7 @@ class SideNotes extends StatelessWidget {
                   textScaleFactor: AppDimension.textScaleFactor(context),
                   maxLines: 3,
                   style: TextStyle(
-                    fontSize: AppDimension.height(16, context),
+                    fontSize: AppDimension.height(14, context),
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -70,7 +107,7 @@ class SideNotes extends StatelessWidget {
                   textScaleFactor: AppDimension.textScaleFactor(context),
                   maxLines: 3,
                   style: TextStyle(
-                    fontSize: AppDimension.height(16, context),
+                    fontSize: AppDimension.height(14, context),
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -85,7 +122,7 @@ class SideNotes extends StatelessWidget {
                   textScaleFactor: AppDimension.textScaleFactor(context),
                   maxLines: 3,
                   style: TextStyle(
-                    fontSize: AppDimension.height(16, context),
+                    fontSize: AppDimension.height(14, context),
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -95,6 +132,6 @@ class SideNotes extends StatelessWidget {
           ),
         )
       ],
-    );
-  }
+    )
+  ];
 }
--- lib/features/second_milestone/presentation/widgets/sliding_text_with_dots_indicator.dart
+++ lib/features/second_milestone/presentation/widgets/sliding_text_with_dots_indicator.dart
@@ -3,14 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:upwork_hassan/core/utils/app_dimension.dart';
 
-class SlidingTextWithDots extends StatefulWidget {
-  const SlidingTextWithDots({Key? key}) : super(key: key);
-
+class SlidingTextWithDots extends StatelessWidget {
   @override
-  State<SlidingTextWithDots> createState() => _SlidingTextWithDotsState();
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 800) {
+          // Desktop layout
+          return DesktopSlidingTextWithDots();
+        } else {
+          // Mobile layout
+          return MobileSlidingTextWithDots();
+        }
+      },
+    );
+  }
 }
 
-class _SlidingTextWithDotsState extends State<SlidingTextWithDots> {
+class MobileSlidingTextWithDots extends StatefulWidget {
+  const MobileSlidingTextWithDots({Key? key}) : super(key: key);
+
+  @override
+  State<MobileSlidingTextWithDots> createState() =>
+      _MobileSlidingTextWithDotsState();
+}
+
+class _MobileSlidingTextWithDotsState extends State<MobileSlidingTextWithDots> {
   int _activeDotIndex = 0;
   CarouselController _carouselController = CarouselController();
 
@@ -28,7 +46,152 @@ class _SlidingTextWithDotsState extends State<SlidingTextWithDots> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            TopIndicator(activeDotIndex: _activeDotIndex),
+            TopIndicator(
+              activeDotIndex: _activeDotIndex,
+              effect: ScrollingDotsEffect(),
+            ),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if (_activeDotIndex > 0) {
+                      setActiveDotIndex(_activeDotIndex - 1);
+                      _carouselController.previousPage();
+                    } else {
+                      setActiveDotIndex(11);
+                      _carouselController.jumpToPage(11);
+                    }
+                  },
+                  child: Container(
+                    height: AppDimension.height(50, context),
+                    width: AppDimension.height(50, context),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(
+                        AppDimension.height(25, context),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: AppDimension.width(1, context),
+                          blurRadius: AppDimension.width(3, context),
+                          offset: const Offset(1, 1),
+                        ),
+                      ],
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.chevron_left,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: AppDimension.width(5, context)),
+                GestureDetector(
+                  onTap: () {
+                    if (_activeDotIndex < 11) {
+                      setActiveDotIndex(_activeDotIndex + 1);
+                      _carouselController.nextPage();
+                    } else {
+                      setActiveDotIndex(0);
+                      _carouselController.jumpToPage(0);
+                    }
+                  },
+                  child: Container(
+                    height: AppDimension.height(50, context),
+                    width: AppDimension.height(50, context),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(
+                        AppDimension.height(25, context),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: AppDimension.width(1, context),
+                          blurRadius: AppDimension.width(3, context),
+                          offset: const Offset(1, 1),
+                        ),
+                      ],
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.chevron_right,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(height: AppDimension.height(20, context)),
+        CarouselSlider.builder(
+          carouselController: _carouselController,
+          options: CarouselOptions(
+            height: AppDimension.height(500, context),
+            aspectRatio: 16 / 9,
+            viewportFraction: 1,
+            autoPlayAnimationDuration: const Duration(milliseconds: 600),
+            onPageChanged: (index, reason) {
+              setActiveDotIndex(index);
+            },
+          ),
+          itemBuilder: (_, index, realIndex) {
+            return Container(
+              width: AppDimension.height(500, context),
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.circular(AppDimension.height(40, context)),
+                image: const DecorationImage(
+                  fit: BoxFit.fitHeight,
+                  image: AssetImage('assets/images/sliding_text1.png'),
+                ),
+              ),
+            );
+          },
+          itemCount: 12,
+        ),
+      ],
+    );
+  }
+}
+
+class DesktopSlidingTextWithDots extends StatefulWidget {
+  const DesktopSlidingTextWithDots({Key? key}) : super(key: key);
+
+  @override
+  State<DesktopSlidingTextWithDots> createState() =>
+      _DesktopSlidingTextWithDotsState();
+}
+
+class _DesktopSlidingTextWithDotsState
+    extends State<DesktopSlidingTextWithDots> {
+  int _activeDotIndex = 0;
+  CarouselController _carouselController = CarouselController();
+
+  void setActiveDotIndex(int index) {
+    setState(() {
+      _activeDotIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TopIndicator(
+              activeDotIndex: _activeDotIndex,
+              effect: _buildCustomEffect(context),
+            ),
             Row(
               children: [
                 GestureDetector(
@@ -141,45 +304,48 @@ class _SlidingTextWithDotsState extends State<SlidingTextWithDots> {
 }
 
 class TopIndicator extends StatelessWidget {
-  const TopIndicator({
-    super.key,
-    required int activeDotIndex,
-  }) : _activeDotIndex = activeDotIndex;
+  TopIndicator({super.key, required int activeDotIndex, required effect})
+      : _activeDotIndex = activeDotIndex,
+        _effect = effect;
 
   final int _activeDotIndex;
+  final _effect;
 
   @override
   Widget build(BuildContext context) {
-    var customizableEffect = CustomizableEffect(
-      spacing: AppDimension.width(2, context),
-      activeDotDecoration: DotDecoration(
-        color: Colors.black,
-        width: AppDimension.height(20, context),
-        height: AppDimension.height(20, context),
-        borderRadius: BorderRadius.circular(
-          AppDimension.height(10, context),
-        ),
-        dotBorder: DotBorder(
-          color: Colors.black,
-          width: AppDimension.height(1, context),
-        ),
-      ),
-      dotDecoration: DotDecoration(
-        borderRadius: BorderRadius.circular(
-          AppDimension.height(10, context),
-        ),
-        width: AppDimension.height(20, context),
-        height: AppDimension.height(20, context),
-        dotBorder: DotBorder(
-          color: Colors.black,
-          width: AppDimension.height(1, context),
-        ),
-      ),
-    );
     return AnimatedSmoothIndicator(
       activeIndex: _activeDotIndex,
       count: 12,
-      effect: ScrollingDotsEffect(),
+      effect: _effect,
     );
   }
+}
+
+IndicatorEffect _buildCustomEffect(context) {
+  return CustomizableEffect(
+    spacing: AppDimension.width(2, context),
+    activeDotDecoration: DotDecoration(
+      color: Colors.black,
+      width: AppDimension.height(20, context),
+      height: AppDimension.height(20, context),
+      borderRadius: BorderRadius.circular(
+        AppDimension.height(10, context),
+      ),
+      dotBorder: DotBorder(
+        color: Colors.black,
+        width: AppDimension.height(1, context),
+      ),
+    ),
+    dotDecoration: DotDecoration(
+      borderRadius: BorderRadius.circular(
+        AppDimension.height(10, context),
+      ),
+      width: AppDimension.height(20, context),
+      height: AppDimension.height(20, context),
+      dotBorder: DotBorder(
+        color: Colors.black,
+        width: AppDimension.height(1, context),
+      ),
+    ),
+  );
 }
